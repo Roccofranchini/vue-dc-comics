a57a2149ec1d6aef3c5e6ad769e2f6ede6b3a6aa
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-06 16:30:37 +0200

    header-content-added

---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-50">
+					<img src="../assets/img/dc-logo.png" alt="" />
+				</div>
+				<div class="col-50">
+					<ul>
+						<li><a href="">CHARACTERS</a></li>
+						<li><a href="" class="active">COMICS</a></li>
+						<li><a href="">MOVIES</a></li>
+						<li><a href="">TV</a></li>
+						<li><a href="">GAMES</a></li>
+						<li><a href="">COLLECTIBLES</a></li>
+						<li><a href="">VIDEOS</a></li>
+						<li><a href="">FANS</a></li>
+						<li><a href="">NEWS</a></li>
+						<li><a href="">SHOP</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "Header",
+	props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+	height: 100px;
+}
+
+.container {
+	height: 100%;
+}
+
+.row {
+	align-items: center;
+	height: 100%;
+}
+
+img {
+	height: 60px;
+}
+
+ul {
+	display: flex;
+	font-weight: 600;
+}
+
+li {
+	padding: 0 10px;
+	font-size: 10px;
+}
+
+.active {
+	color: #0282f9;
+}
+</style>
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,27 @@
+<template>
+	<section id="content">
+		<div class="container">
+			<h3>
+				-- Content goes here --
+			</h3>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "Jumbo",
+};
+</script>
+
+<style scoped lang="scss">
+#content {
+	background-color: black;
+}
+
+h3 {
+	color: white;
+	padding: 40px;
+	font-size: 30px;
+}
+</style>
