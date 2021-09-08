a82858003ea60738fa8ad7a291f720103131f808
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-08 15:46:23 +0200

    series-added

---
+++ src/components/CardSerie.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="col">
+		<img :src="serie.thumb" alt="" />
+		<figcaption>{{ serie.series }}</figcaption>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CardSerie",
+	props: ["serie"],
+};
+</script>
+
+<style scoped lang="scss">
+figcaption {
+	color: white;
+	margin: 10px 0;
+	text-align: center;
+}
+
+.col {
+	flex-basis: calc(100% / 6);
+	padding: 0 10px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	img {
+		width: 60%;
+	}
+}
+</style>
